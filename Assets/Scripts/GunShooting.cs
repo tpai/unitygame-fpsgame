f977cfa0cee854c2cf5cc2d5cff75048f52204b8
tonypai <tony77794@gmail.com>
2015-04-26 14:04:38 +0800

    射擊延遲腳本

---
+++ Assets/Scripts/GunShooting.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunShooting : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float gunSpeed = .2f;
+	bool holdShoot = false;
+
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			if (holdShoot == false)
+				StartCoroutine("ShootBullet");
+		}
+	}
+
+	IEnumerator ShootBullet () {
+		holdShoot = true;
+		Instantiate(bulletPrefab, transform.Find ("Top").position, transform.Find ("Top").rotation);
+		yield return new WaitForSeconds (gunSpeed);
+		holdShoot = false;
+	}
+}
