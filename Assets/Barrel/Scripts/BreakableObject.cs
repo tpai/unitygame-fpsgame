9fefdc1dd068dd1e21c3b9122db715f5f300a43a
tonypai <tony77794@gmail.com>
2015-04-30 01:05:20 +0800

    新增目標木桶且具備破碎效果

---
+++ Assets/Barrel/Scripts/BreakableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableObject : MonoBehaviour {
+
+	void Broken (Vector3 point) {
+
+		Debug.Log (point);
+
+		transform.Find ("Barrel").GetComponent<MeshRenderer> ().enabled = false;
+
+		for (int i=1;i<transform.childCount;i++) {
+			transform.GetChild(i).gameObject.GetComponent<Rigidbody>().useGravity = true;
+			transform.GetChild(i).gameObject.GetComponent<MeshCollider>().enabled = true;
+			transform.GetChild(i).gameObject.GetComponent<Rigidbody>().AddExplosionForce(500f, point, 100f);
+//			transform.GetChild(i).gameObject.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Barrel/Scripts/HitObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitObject : MonoBehaviour {
+
+	void OnCollisionEnter (Collision coll) {
+		if (coll.collider.name.Contains("Bullet")) {
+			transform.parent.SendMessage("Broken", coll.contacts[0].point);
+			Destroy (gameObject);
+		}
+	}
+}
