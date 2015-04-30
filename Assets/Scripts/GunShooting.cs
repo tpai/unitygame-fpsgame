75b142dfd5a48740d147ef7392167ba93083a5ad
tonypai <tony77794@gmail.com>
2015-04-30 14:37:28 +0800

    實作蹲下

--- Assets/Scripts/GunShooting.cs
+++ Assets/Scripts/GunShooting.cs
@@ -4,6 +4,7 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class GunShooting : MonoBehaviour {
 
+	public CharacterController chrCtrler;
 	public FirstPersonController fpsCtrler;
 	public Animator envAnim;
 	public Animator gunAnim;
@@ -16,26 +17,38 @@ public class GunShooting : MonoBehaviour {
 
 		if (Input.GetMouseButtonDown (1)) {
 			isAiming = true;
-			fpsCtrler.RunSpeed = 5f;
 			envAnim.SetBool("aim", true);
 			gunAnim.SetBool("aim", true);
 		}
 		if (Input.GetMouseButtonUp (1)) {
-			fpsCtrler.RunSpeed = 10f;
 			isAiming = false;
 			envAnim.SetBool("aim", false);
 			gunAnim.SetBool("aim", false);
 		}
 
-		if (!isAiming && Input.GetKey (KeyCode.LeftShift)) {
-			if (Input.GetAxis ("Vertical") < 0f);
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			chrCtrler.height = .9f;
+			envAnim.SetBool("crouch", true);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftControl)) {
+			chrCtrler.height = 1.8f;
+			envAnim.SetBool("crouch", false);
+		}
+
+		if (!isAiming && !Input.GetKey (KeyCode.LeftControl) && Input.GetKey (KeyCode.LeftShift)) {
+			if (Input.GetAxis ("Vertical") <= 0f && Input.GetAxis ("Horizontal") == 0f) {
+				fpsCtrler.RunSpeed = 5f;
+				gunAnim.SetBool("sprint", false);
+			}
 			else {
+				fpsCtrler.RunSpeed = 10f;
 				gunAnim.SetBool("sprint", true);
 				if (isAiming)
 					gunAnim.SetBool("aim", false);
 			}
 		}
 		else {
+			fpsCtrler.RunSpeed = 5f;
 			gunAnim.SetBool("sprint", false);
 
 			if (Input.GetMouseButton (0))
