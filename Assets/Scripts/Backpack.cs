e983b76c9c7d3a1072c85c66bdfdde37e5b43163
tonypai <tony77794@gmail.com>
2015-05-10 00:40:34 +0800

    近戰、主武及副武123切換 (附音效)

---
+++ Assets/Scripts/Backpack.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backpack : MonoBehaviour {
+
+	[SerializeField] private AudioSource audio;
+	public enum HandHold { MeleeWeapon, MainWeapon, SecondaryWeapon };
+	public HandHold holdingWeapon;
+
+	void Start () {
+		WeaponCheck ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			holdingWeapon = HandHold.MeleeWeapon;
+			WeaponCheck ();
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			holdingWeapon = HandHold.SecondaryWeapon;
+			WeaponCheck ();
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			holdingWeapon = HandHold.MainWeapon;
+			WeaponCheck ();
+		}
+	}
+	
+	void WeaponCheck () {
+		PutDownAllWeapons ();
+		switch (holdingWeapon) {
+			case HandHold.MeleeWeapon:
+				transform.Find ("MeleeWeapon").gameObject.SetActive(true);
+			break;
+			case HandHold.MainWeapon:
+				transform.Find ("MainWeapon").gameObject.SetActive(true);
+			break;
+			case HandHold.SecondaryWeapon:
+				transform.Find ("SecondaryWeapon").gameObject.SetActive(true);
+			break;
+		}
+		audio.Play ();
+	}
+
+	void PutDownAllWeapons () {
+		foreach (Transform weapon in transform) {
+			weapon.gameObject.SetActive(false);
+		}
+	}
+}
