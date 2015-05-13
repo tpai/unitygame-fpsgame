6d464dd6d0a49bcf10973528504ba90a16184da2
tonypai <tony77794@gmail.com>
2015-05-14 00:19:36 +0800

    實作玩家登入、實時位置旋轉

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkManager : MonoBehaviour {
+
+	[SerializeField] Text connectionText;
+	[SerializeField] InputField messageText;
+
+	[SerializeField] Camera camera;
+	[SerializeField] AudioListener audioListener;
+	[SerializeField] Transform loginPanel;
+	[SerializeField] InputField playerName;
+
+	[SerializeField] Transform[] spawnPoints;
+
+	GameObject player;
+	Queue<string> messages;
+	const int messageCount = 7;
+	PhotonView photonView;
+
+	void Start () {
+		photonView = GetComponent<PhotonView> ();
+		messages = new Queue<string> (messageCount);
+		
+		PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings ("1.0");
+		StartCoroutine ("UpdateConnectionText");
+	}
+
+	IEnumerator UpdateConnectionText () {
+		while (true) {
+			connectionText.text = PhotonNetwork.connectionStateDetailed.ToString ();
+			yield return null;
+		}
+	}
+
+	void OnJoinedLobby () {
+		loginPanel.gameObject.SetActive (true);
+	}
+
+	public void JoinRoom () {
+		PhotonNetwork.player.name = playerName.text;
+		RoomOptions roomOptions = new RoomOptions () { isVisible = true, maxPlayers = 10 };
+		PhotonNetwork.JoinOrCreateRoom ("Default", roomOptions, TypedLobby.Default);
+	}
+
+	void OnJoinedRoom () {
+		StopCoroutine ("UpdateConnectionText");
+		connectionText.text = "";
+		StartSpawnProcess (0f);
+	}
+
+	void StartSpawnProcess (float respawnTime) {
+		camera.enabled = true;
+		audioListener.enabled = true;
+		StartCoroutine ("SpawnPlayer", respawnTime);
+	}
+
+	IEnumerator SpawnPlayer (float respawnTime) {
+		yield return new WaitForSeconds(respawnTime);
+		
+		int index = Random.Range (0, spawnPoints.Length);
+		player = PhotonNetwork.Instantiate (
+			"Player", 
+			spawnPoints [index].position, 
+			spawnPoints [index].rotation, 
+			0
+		);
+//		player.GetComponent<PlayerNetworkMover> ().RespawnMe += StartSpawnProcess;
+//		player.GetComponent<PlayerNetworkMover> ().SendNetworkMessage += AddMessage;
+		camera.enabled = false;
+		audioListener.enabled = false;
+		loginPanel.gameObject.SetActive (false);
+		
+		AddMessage ("Spawned player: " + PhotonNetwork.player.name);
+	}
+	
+	void AddMessage(string message) {
+		photonView.RPC ("AddMessage_RPC", PhotonTargets.All, message);
+	}
+	
+	[RPC]
+	void AddMessage_RPC(string message) {
+		messages.Enqueue (message);
+		if(messages.Count > messageCount) {
+			messages.Dequeue();
+		}
+		messageText.text = "";
+		foreach (string m in messages) {
+			messageText.text += m + "\n";
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerNetworkMover.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerNetworkMover : MonoBehaviour {
+
+	public float posSpeed = 10f;
+	public float rotSpeed = 10f;
+
+	Vector3 m_NetworkedPosition;
+	Quaternion m_NetworkedRotation;
+
+	void Start () {
+		if (GetComponent<PhotonView>().isMine) {
+			GetComponent<FirstPersonController> ().enabled = true;
+			GetComponentInChildren<Camera> ().enabled = true;
+			GetComponentInChildren<AudioListener> ().enabled = true;
+		}
+	}
+
+	void Update () {
+		if (!GetComponent<PhotonView>().isMine) {
+			UpdateNetworkedPosition ();
+			UpdateNetworkedRotation ();
+		}
+	}
+
+	void UpdateNetworkedPosition () {
+		transform.position = Vector3.Lerp (transform.position, m_NetworkedPosition, Time.deltaTime * posSpeed);
+	}
+
+	void UpdateNetworkedRotation () {
+		transform.rotation = Quaternion.Lerp (transform.rotation, m_NetworkedRotation, Time.deltaTime * rotSpeed);
+	}
+
+	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		SerializeState (stream, info);
+	}
+
+	void SerializeState (PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			stream.SendNext (transform.position);
+			stream.SendNext (transform.rotation);
+		}
+		else {
+			m_NetworkedPosition = (Vector3)stream.ReceiveNext ();
+			m_NetworkedRotation = (Quaternion)stream.ReceiveNext ();
+		}
+	}
+}
